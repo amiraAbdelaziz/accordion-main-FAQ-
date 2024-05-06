9f0fea83d54f4cc3b82377769b8a28513cb9bc25
amiraAbdelaziz <133931390+amiraAbdelaziz@users.noreply.github.com>
2024-05-06 10:38:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let plus = document.querySelectorAll("[name='plus']")
+let p = document.querySelectorAll("p")
+
+function display() {
+  plus.forEach(function(e) {
+    e.addEventListener('click', function() {
+      let span = e.parentElement.parentElement.lastElementChild
+      if (this.src.includes("icon-plus.svg")) {
+        this.src = this.src.replace("icon-plus.svg", "icon-minus.svg");
+        span.style.display = "block"
+      } else if (this.src.includes("icon-minus.svg")) {
+        this.src = this.src.replace("icon-minus.svg", "icon-plus.svg");
+        span.style.display = "none"
+      }
+    });
+  })
+}
+
+display()
